574eb88704e71222a77b78f3f390b6a9e9c6a340
Myunghee <audgml0087@naver.com>
2023-06-01 10:00:01 +0900

    Divide Add

--- Project1/cal.cpp
+++ Project1/cal.cpp
@@ -1,4 +1,8 @@
 class Cal
 {
-	
+public:
+	int getDivide(int a, int b)
+	{
+		return a / b;
+	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -3,4 +3,12 @@
 TEST(TestCaseName, TestName) {
 	Cal* cal = new Cal();
     EXPECT_TRUE(cal != nullptr);
+	delete cal;
+}
+TEST(TestCaseName, TestDivide) {
+	Cal* cal = new Cal();
+	EXPECT_TRUE(cal != nullptr);
+
+	EXPECT_EQ(2, cal->getDivide(4, 2));
+	delete cal;
 }
