318fdd69c323e1d0c49d42b5a3c73dc4137da22a
chanwoo <pakchanwoo@gmail.com>
2023-06-01 12:51:44 +0900

    fix conflict

--- Project1/cal.cpp
+++ Project1/cal.cpp
@@ -1,8 +1,31 @@
 class Cal
 {
 public:
-	int getMinus(int a, int b)
+		int getMinus(int a, int b)
 	{
 		return a - b;
-	}
-};
+			int getDivide(int a, int b)
+		{
+			return a / b;
+		}
+
+		int getSum(const int a, const int b)
+		{
+			return a + b;
+		}
+
+		int getGop(const int a, const int b)
+		{
+			return a * b;
+		}
+
+		int getZegop(int a)
+		{
+			return a * a;
+		}
+
+		int getSumSum(const int a, const int b, const int c)
+		{
+			return getSum(a, b) + c;
+		}
+	};
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -2,9 +2,37 @@
 #include "../Project1/cal.cpp"
 TEST(TestCaseName, TestName) {
 	Cal* cal = new Cal();
-    EXPECT_TRUE(cal != nullptr);
+	EXPECT_TRUE(cal != nullptr);
 }
 TEST(TestCaseName, TestMinus) {
 	Cal* cal = new Cal();
 	EXPECT_EQ(30, cal->getMinus(100, 70));
+		delete cal;
+}
+
+TEST(TestCaseName, TestDivide) {
+	Cal* cal = new Cal();
+	EXPECT_TRUE(cal != nullptr);
+
+	EXPECT_EQ(2, cal->getDivide(4, 2));
+	delete cal;
+}
+TEST(TestCaseName, TestSum) {
+	Cal* cal = new Cal();
+	EXPECT_EQ(3, cal->getSum(1, 2));
+}
+
+TEST(TestCaseName, TestZegop) {
+	Cal* cal = new Cal();
+	EXPECT_EQ(9, cal->getZegop(3));
+}
+
+TEST(TestCaseName, TestGop) {
+	Cal* cal = new Cal();
+	EXPECT_EQ(9, cal->getGop(1, 9));
+}
+
+TEST(TestCaseName, TestSumSum) {
+	Cal* cal = new Cal();
+	EXPECT_EQ(17, cal->getSumSum(1, 9, 7));
 }
